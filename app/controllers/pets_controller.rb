ee214ddce96b1c4e37b2fb5554dda2439eba93ec
Dianna Tingg <dtingg@gmail.com>
2019-11-05 09:55:24 -0800

    added responds with array of pet hashes test

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,12 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    # @pets = Pet.all
+    render json: { ready_for_lunch: "yassss" }, status: :ok
   end
-
-
-
+  
   private
-
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
-    end
+  
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -8,6 +8,20 @@ describe PetsController do
       expect(response.header['Content-Type']).must_include 'json'
       must_respond_with :ok
     end
+    
+    it "responds with an array of pet hashes" do
+      # Act
+      get pets_path
+      
+      # Get the body of the response
+      body = JSON.parse(response.body)
+      
+      # Assert
+      expect(body).must_be_instance_of Array
+      body.each do |pet|
+        expect(pet).must_be_instance_of Hash
+        expect(pet.keys.sort).must_equal ["age", "human", "id", "name"]
+      end
+    end
   end
-  
 end
